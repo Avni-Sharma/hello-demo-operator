b66caf2b3da255c249d70788eee57ad12d05b57d
Avni Sharma <16avnisharma@gmail.com>
2020-05-26 23:44:06 +0530

    Generate Hello api

---
+++ pkg/apis/hello/group.go
@@ -0,0 +1,6 @@
+// Package hello contains hello API versions.
+//
+// This file ensures Go source parsers acknowledge the hello package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package hello
